166c1e7003046f0b51eb8d2c6de04fd31b962401
vluu11 <luuvuong11@gmail.com>
2024-11-18 04:41:13 -0800

    created queries and mutations

---
+++ client/src/utils/mutations.ts
@@ -0,0 +1,65 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String, $username: String, $password: String!) {
+    login(email: $email, username: $username, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: BookInput!) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
