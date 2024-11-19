1c6ce6f63a02ef34e81866941d665d077ed11100
vluu11 <luuvuong11@gmail.com>
2024-11-19 13:26:25 -0800

    added fileUrltopath

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,13 @@ import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './services/auth.js';
 
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = path.dirname(__filename);
+
 const server = new ApolloServer({
   typeDefs,
   resolvers
