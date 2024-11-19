b11f12f08c1e58b5bc7b81856d43c7960b2d2e9c
vluu11 <luuvuong11@gmail.com>
2024-11-19 13:27:05 -0800

    added extra code

--- server/src/server.ts
+++ server/src/server.ts
@@ -36,10 +36,10 @@ const startApolloServer = async () => {
   ));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
 
     app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
 
