88692c0fdce9745350208b869e9afe5e02a36b80
vluu11 <luuvuong11@gmail.com>
2024-11-19 13:22:26 -0800

    comment

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || '';
+// const MONGODB_URI = process.env.MONGODB_URI || '';
 console.log(process.env);
 const db = async (): Promise<typeof mongoose.connection> => {
   try {
