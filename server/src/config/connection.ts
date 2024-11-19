def2ff3465c0d5f2c59c706c0961416dc5990a66
vluu11 <luuvuong11@gmail.com>
2024-11-19 13:20:27 -0800

    added uri

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -7,7 +7,7 @@ const MONGODB_URI = process.env.MONGODB_URI || '';
 console.log(process.env);
 const db = async (): Promise<typeof mongoose.connection> => {
   try {
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect("mongodb+srv://vluu11:mypassword123@cluster0.vsdzq.mongodb.net/googlebooks?retryWrites=true&w=majority");
     console.log('Database connected.');
     return mongoose.connection;
   } catch (error) {
